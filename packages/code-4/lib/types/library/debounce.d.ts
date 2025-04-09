2fe875a5fb2dff82f083a5c82f1d17cd36c32777
Chad Neff <ccneff@gmail.com>
2025-04-08 21:39:57 -0700

    fresh build of code-4 and moxi edit

--- packages/code-4/lib/types/library/debounce.d.ts
+++ packages/code-4/lib/types/library/debounce.d.ts
@@ -1,6 +1,11 @@
-export declare function debounce<T extends unknown[], U>(callback: (...args: T) => PromiseLike<U> | U, wait: number): (...args: T) => Promise<U>;
 export declare const getHashCode: (str?: string) => number;
+interface DebounceFuncInfo {
+    lastCall: number;
+    waitMs: number;
+    timer: number;
+}
 export declare function trueDebounce(func: () => void, waitMs?: number): void;
 export declare namespace trueDebounce {
-    var funcDictionary: {};
+    var funcDictionary: Record<string, DebounceFuncInfo>;
 }
+export {};

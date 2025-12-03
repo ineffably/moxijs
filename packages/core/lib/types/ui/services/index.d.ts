358121122c35fba5365bf7539b5bdd633047d143
Chad Neff <ccneff@gmail.com>
2025-12-02 18:32:48 -0800

    refactor(core): remove unused UI types from core package
    - Deleted obsolete UI type definitions and layout engine types from core library.
    - Cleaned up core index exports accordingly.

--- packages/core/lib/types/ui/services/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { LayoutEngine, SizeConstraints } from './layout-engine';
-export { FormStateManager, FormStateConfig } from './form-state-manager';
-export { TextInputHandler, TextInputHandlerConfig } from './text-input-handler';
-export { ThemeApplier, ComponentState } from './theme-applier';
